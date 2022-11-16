3a2a9df95551f4ec255654feffb86e8d4ee7f404
minseung-gang <strause1@nate.com>
2022-11-16 10:30:02 +0900

    메인홈페이지 html 및 js 수정

--- static/JS/com.js
+++ static/JS/com.js
@@ -1,29 +1,29 @@
 $(document).ready(function () {
-    listing();
+  listing();
 });
 
 function listing() {
-    $.ajax({
-        type: 'GET',
-        url: '/detail',
-        data: {},
-        success: function (response) {
-            let rows = response['data']
-            for (let i = 0; i < rows.length; i++) {
-                let image = rows[i]['image']
-                let title = rows[i]['title']
-                let description = rows[i]['description']
-
-                let temp_html = `<ul>
+  $.ajax({
+    type: "GET",
+    url: "/detail",
+    data: {},
+    success: function (response) {
+      let rows = response["data"];
+      for (let i = 0; i < rows.length; i++) {
+        let image = rows[i]["img"];
+        let title = rows[i]["title"];
+        let description = rows[i]["description"];
+        let order = rows[i]["order"];
+        let temp_html = `<ul>
                                     <li>
                                        <div class="image"><img src="${image}"></div>
                                        <div class="title"><h5>${title}</h5></div>
                                        <div class="description">${description}</div>
-                                       <div><a href="/detail"></a>글 상세보기</div>
+                                       <div><a href="/detail/${order}">글 상세보기</a></div>
                                     </li>
-                                 </ul>`
-                $('#card').append(temp_html)
-            }
-        }
-    })
+                                 </ul>`;
+        $("#card").append(temp_html);
+      }
+    },
+  });
 }
